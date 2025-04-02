d60bd38799e71c04de4cc7c2c65fe3c5d37d563b
Sid <aishwaryakumari@Aishwaryas-MacBook-Pro.local>
2025-04-02 09:11:19 +0530

    added files

---
+++ public_html/assets/js/portfolio.js
@@ -0,0 +1,25 @@
+/*!
+=========================================================
+* Portfolio Landing page
+=========================================================
+
+*/
+
+// smooth scroll
+$(document).ready(function(){
+    $(".navbar .nav-link").on('click', function(event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function(){
+                window.location.hash = hash;
+            });
+        } 
+    });
+});
